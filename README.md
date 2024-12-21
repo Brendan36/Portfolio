add0fe6a7f42e5071a782a65d349782821b175dd
BE <142072248+Brendan36@users.noreply.github.com>
2024-12-21 21:30:59 +0700

    updated files and readme

--- README.md
+++ README.md
@@ -4,19 +4,61 @@ The purpose of this webapp to showcase my current and future projects.
 
 ## Applications currently in the production phase
 ### To-do list
+Purpose:\
 This is a simple To-do web app that allows a user to create, update, and delete tasks.
 ### Automate Excel
+Purpose:\
 This is a web app that allows a user to upload an Excel file and automatically fill in the data into a template Excel file.
 ### AI Avatar
+Purpose:\
 This is an app to create an
-
+### Typing Analyzer
+Purpose:\
+A program that monitors the computer keyboard and returns the most typed words of the session
+### Webcam Motion Detector
+Purpose:\
+A program that monitors the computer webcam and sends an email when a new object enters the view.
+### Typing Analyzer
+A program that monitors the computer keyboard and returns the most typed words of the session.
+### Monitoring Data
+Purpose:\
+A program that monitors a particular website and periodically records the data to a remote server.
+### Visualizing Data
+Purpose:\
+A program that monitors a particular website and periodically records the data to a remote server.
+### Good News
+Purpose:\
+A website that gets data from a news API and uses sentiment analysis to publish only positive news.
+### Climate Change API
+Purpose:\
+A REST API built with Python and Flask to serve historical weather data for various cities, and to analyze the data to 
+calculate the rate of climate change.
+### Weather Forecast
+Purpose:\
+A data web app that visualizes the weather forecast for the next five days of any city, compared the same day of the 
+previous years, and forcast for the years to come.
+### Online Store
+Purpose:\
+A Django online store app where users can place orders directly with sustainable products from sellers who are 
+transparent about their sustainability practices.
 ## Future plans
-### *Automated trading*
+### *Tinkerbell*
+Purpose:\
+A chatbot that monitors information gathered from a website with an indicator of the probability of the information 
+being true.
+### *moneyBE*
 Purpose:\
 To enable a user to automatically trade stocks, bonds, and options using predictive analytics.
 Allow users to set up a trading strategy and let the app execute the strategy on their behalf.
 Allow users to monitor and analyze their trading performance.
-
+With a focus on highly profitable businesses with alternative practices and projects which are not only transparent 
+about their sustainability, but also transparent about their economic impact
+### *TRUST*
+Purpose:\
+A web app that exposes the conflicts of interest which exist between regulatory bodies and industries. 
+The app will include visualizations of the correlation between enabling sustainability and the economic impact while
+moving away from the current unsustainable universally accepted commercialization and exploitation of the world's 
+natural resources.
 ### *How does my pet see the world*
 Purpose:\
 For those who are curious about how a pet sees the world, the app applies a grayscale filter to images to create a simulation of how a pet might see the world.
--- main.py
+++ main.py
@@ -4,42 +4,100 @@ from streamlit import empty
 
 st.set_page_config(layout="wide")
 
+st.title("BE")
+# st.write("Hi, I’m Brendan!\n")
 col1, col2 = st.columns(2)
 with col1:
     st.image('images/blue smiley.jpg')
 with col2:
-    st.title("BE")
     content = """
     Hi, I’m Brendan!\n
     I’m an endlessly curious, imaginative hands-on creator who loves to tinker with Python and R 
     to uncover deeper insights through data.\n
-
+    
     My growing passion for animal welfare, environmental stewardship, and human rights, 
     has inspired me to develop tools designed to illuminate the subtle links between our 
     everyday habits and their global ripple effects.\n
     My goal is to help us recognize how even small actions can influence the world around us... 
     these same actions which are either helping it thrive or contributing to its decline.\n
-
+    
     By sharpening our self-awareness, we can better understand how individual choices add up and, 
     more importantly, what steps we can take to inspire positive change. 
     I’m eager for these tools to become a starting point for meaningful transformation
     —but that can only happen when people put them to use.\n
-
-    While working together, we can all broaden our understanding and fuel the shifts that matter most.\n
+   
     If you have ideas, improvements, or new feature suggestions, I’d love to hear from you!\n
+
+    """
+    st.write(content)
+
+mission_statement = """
+    Mission Statement
+    """
+st.header(mission_statement, divider='rainbow')
+
+blurb = """
+    To inspire global collaboration so that we can all broaden our understanding and fuel the shifts that matter most.\n
     
     Let’s create a better world together!\n
     
     I intend to work with you to build TRUST, an application that:\n
-
-    1. Debunks the correlation between "sustainability" and the actual impact of the commercialization of natural resources on the world.\n
-    2. Improves our understanding of the success stories of collectives of individuals who have extensively researched viable alternatives.\n
-    3. Provides insights into the financial results of industries involved and by virtue, their impact while shining light on the environmental cost... i.e. 3000 people could have eaten for a week by the food that given to 5000 chickens which were fed to 3000 people in 1 day \n
-    4. 
-    10. Demonstrates the power of collective action to change the world.\n
     
+    1. Debunks the correlation between "sustainability" and the actual impact of the commercialization of natural resources on the world.\n
+    2. Improves our understanding of the success stories of collectives and individuals who have extensively researched viable alternatives and have them implemented.\n
+    3. Provides insights into the financial results of industries involved and by virtue, their impact while shining light on the environmental cost... \n
+    4. Enables peer-to-peer collaboration and resource pooling to empower a new sustainable industry for the future. \n
+    5. Demonstrates the power of collective action so we can easily quantify our collective impact and see which changes we are making in our world.\n
+        >>> **WE CANNOT RELY ON OUR CURRENT CAPITALIST DRIVEN WORLD INDUSTRIES TO DO RIGHT BY US - it's simply not profitable for them.**\n
+
     """
-    st.info(content)
+st.write(blurb)
+dyk1 = """
+    A commercially packed 1.5kg chicken requires 5kg of feed.\n
+    KFC's Thai restaurant chain serves approximately 800,000 pieces of fried chicken daily, or 292 million pieces per year. \n
+    (assuming 8pieces = 1 chicken) - That's equivalent to 100,000 chickens per day or 3,000,000 chickens per year. \n
+    assuming 1 chicken = 1.5 kg of meat\n
+    it takes 550,000kg of feed (500 ton) to produce the 100,000 (150 ton) chickens that are sold daily.\n
+    \n
+    350 tons of produce was lost in the conversion process.\n
+    \n
+    **Is this sustainable?**
+
+    """
+
+dyk2 = """
+    To produce 1 kg of beef: 13–15 kg of feed (forage + grain) is required.\n
+    15,000 liters of water (for feed production, drinking water, and processing).\n
+    -> **! That's 15,000 liters of water for 1 kg of beef !** <-   **WHY DO WE SUPPORT THIS?**\n
+    
+    FYI: These numbers are based on the Average Feed Requirements for rearing cattle:\n
+   
+    We've assumed that 8 kg of feed per 1 kg of live weight gain for a typical beef cattle.
+    Since only 60% of live weight translates to edible meat, the adjusted FCR is approximately:\n
+    8kg feed / 0.6 (meat yield factor) = 13.33kg feed for 1kg beef.
+    \n
+    **Is this sustainable?**
+    """
+st.header("Sad state of affairs...", divider='rainbow')
+st.write("**MOST** commercial industries are not sustainable...")
+st.write("**PEOPLE** should have access to other options - I want to change that...")
+st.write("**ARE** we really doing the best we can? - I want to help us realise what we are doing...")
+st.write("**IGNORANT** is what we are if, we blindly trust and support of commercial industries (many of them only perpetuate the problems)...")
+st.write("**TO** become sustainable, we need to inspire others to take action...")
+st.write("**CHANGE** is the only realistic option we have left - it's never too late to take action...")
+st.write("**IN REALITY** we need to stop supporting industries who are intentionally misleading us...")
+st.subheader("What do you intend to do?", divider='rainbow')
+
+col3, empty_col, col4 = st.columns([1.5, 0.1, 1.5])
+with col3:
+    # st.subheader("Did You Know..?")
+    st.info(dyk2)
+
+with col4:
+    # st.subheader("Did You Know..?")
+    st.info(dyk1)
+
+
 
 content2 = """
     Here are a few of my completed projects:
@@ -49,17 +107,20 @@ st.header(content2)
 df = pd.read_csv('portfolio_data.csv', sep=';')  # read the csv file
 # st.table(df)
 
-col3, empty_col, col4 = st.columns([1.5, 0.1, 1.5])
-with col3:
+col5, empty_col, col6 = st.columns([1.5, 0.1, 1.5])
+with col5:
     for i, row in df[:10].iterrows():
-        st.subheader(row['title'])
+        st.subheader("", divider='rainbow')
+        st.subheader(row['title'], divider='rainbow')
         st.write(row['description'])
         st.image("images/" + row['image'])
         st.write(f"[Source Code]({row['url']})")
 
-with col4:
+with col6:
     for i, row in df[10:20].iterrows():
-        st.subheader(row['title'])
+        st.subheader("", divider='rainbow')
+
+        st.subheader(row['title'], divider='rainbow')
         st.write(row['description'])
         st.image("images/" + row['image'])
         st.write(f"[Source Code]({row['url']})")
@@ -70,7 +131,10 @@ content3 = """
     """
 st.header(content3)
 for i, row in df[20:].iterrows():
+    st.subheader("", divider='rainbow')
     st.subheader(row['title'])
     st.write(row['description'])
     st.image("images/" + row['image'])
+st.subheader("", divider='rainbow')
+
 
